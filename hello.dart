167d2e0959193e70567b49ac9a2d7d4a16623003
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-06-27 19:13:20 +0600

    6/27/25

--- hello.dart
+++ hello.dart
@@ -1,3 +1,14 @@
 void main(){
-  print('Prosenjit');
+  var name='Prosenjit Mondol';
+  var age=25;
+  var arr = [3.5, 3.8, 4.0];
+  print('My name is $name and my age is $age and my current cgpa is ${arr[1]}');
+
+  var semester={
+    '1':'Level 1 Semester 1',
+    '2':'Level 2 Semester 1',
+    '3':'Level 3 Semester 1',
+    '4': 'Level 4 Semester 1'
+  };
+  print('Now I am studying at ${semester['3']}');
 }
