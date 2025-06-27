77c570b183c6242087b95358c3ac649de06cc943
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-06-28 01:52:01 +0600

    6/36/23

--- hello.dart
+++ hello.dart
@@ -3,7 +3,7 @@ void main(){
   var age=25;
   var arr = [3.5, 3.8, 4.0];
   print('My name is $name and my age is $age and my current cgpa is ${arr[1]}');
-
+//list
   var semester={
     '1':'Level 1 Semester 1',
     '2':'Level 2 Semester 1',
@@ -11,4 +11,13 @@ void main(){
     '4': 'Level 4 Semester 1'
   };
   print('Now I am studying at ${semester['3']}');
+
+  // map
+  var bio={
+    'name':'Prosenjit',
+    'age':25,
+    'Branch':'Computer Science & Engineering'
+  };
+  print('The of the student  is ${bio['name']} and his age is ${bio['age']}');
+
 }
