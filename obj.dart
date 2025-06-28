f4766b998e36d3e23bd7254a7de27a979e41cd1f
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-06-28 17:50:40 +0600

    ok

--- obj.dart
+++ obj.dart
@@ -4,8 +4,22 @@ void main(){
   if(name=='Prosenjit'){
     print('He is a legend ');
   }
+  for(var i=0; i<10;i=i+1){
+    print("Dart $i");
+  }
   var list=[3.58,3.41,3.84,3.92];
   for(var i in list){
     print(i);
   }
+
+  var i=0;
+  while(i<10){
+    print(i+1);
+    i+=1;
+  }
+  var j=20;
+  do{
+    print(j+1);
+    j+=1;
+  }while(j<25);
 }
