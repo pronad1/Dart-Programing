cfa2c9674767738cd6cfa8b4f0714e582da5218f
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-06-28 11:56:29 +0600

    6/28/25

--- obj.dart
+++ obj.dart
@@ -1,3 +1,11 @@
 void main(){
   print('Prosenjit are you mad');
+  var name='Prosenjit';
+  if(name=='Prosenjit'){
+    print('He is a legend ');
+  }
+  var list=[3.58,3.41,3.84,3.92];
+  for(var i in list){
+    print(i);
+  }
 }
